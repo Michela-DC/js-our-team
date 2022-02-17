25086297ebaca0083b5b8a899dc3d78cb9013c43
Michela-DC <micheladelconte@outlook.com>
2022-02-17 23:37:53 +0100

    Added function to add new members

--- our-team-base/script.js
+++ our-team-base/script.js
@@ -42,14 +42,20 @@ const team = [
 ];
 console.log(team);
 
+// pusho il nuovo oggetto dentro all'array team usando la funzione per generare nuovi oggetti
+team.push(newMember('New Member', 'Marketing Manager', 'img/new-team-member-01.jpg'));
+team.push(newMember('New Member', 'Project Manager', 'img/new-team-member-02.jpg'));
+team.push(newMember('New Member', 'Data Analyst', 'img/new-team-member-03.jpg'));
+team.push(newMember('New Member', 'Chief Financial Officer', 'img/new-team-member-04.jpg'));
+console.log(team)
+
 // Creo un ciclo for che cicla dentro l'array di oggetti
 for(let i = 0; i < team.length; i++){
     // creo la team card 
     const teamCard =`<div class="team-card">
                         <div class="card-image">
                             <img
-                                src="${team[i].image}"
-                                alt="${team[i].name}"
+                                src="${team[i].image}" 
                             />
                         </div>
                         <div class="card-text">
@@ -57,9 +63,26 @@ for(let i = 0; i < team.length; i++){
                             <p>${team[i].role}</p>
                         </div>
                     </div>`;
-                    
+
     //inserisco la teamCard nel teamContainer in HTML
     teamContainer.innerHTML += `${teamCard}`;   
 }
+//Dentro al ciclo for quando scrivo team[i].image --> con team[i] prendo la posizione dell'array e con .image dico quale proprietà, di quella data posizione di team, voglio vedere
 
 console.log(teamContainer);
+
+//funzione che crea un nuovo oggetto e mi ritorna il nuovo oggetto
+function newMember(nome, ruolo, foto) {
+    // creo un nuovo oggetto
+    const member = {
+        name: nome,
+        role: ruolo,
+        image: foto,
+    }
+
+    return member;
+}
+
+
+
+
